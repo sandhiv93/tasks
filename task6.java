8d9691004c67d1bf1e020ea00eeb61fae2668ee0
sandhiv93 <167868273+sandhiv93@users.noreply.github.com>
2024-05-10 16:53:33 +0530

    Add files via upload

---
+++ task6.java
@@ -0,0 +1,11 @@
+public class task6 {
+    public static void main(String[] args) {
+        // create a string
+        String greet = "Guvi Geek";
+        System.out.println("String: " + greet);
+
+        // get the length of greet
+        int length = greet.length();
+        System.out.println("Length: " + length);
+    }
+}
